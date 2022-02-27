5070e8641781a04c589f89bd2cd545dee29f491b
Despect <el-despecto@ya.ru>
2022-02-28 00:47:09 +0900

    12

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PostList, PostDetailView, PostCreateView, PostDeleteView, PostUpdateView
+from .views import PostList, PostDetailView, PostCreateView, PostDeleteView, PostUpdateView, PostSearch
 
 urlpatterns = [
     path('', PostList.as_view()),
+    path('news_search/', PostSearch.as_view(), name='news_search'),
     path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('create/', PostCreateView.as_view(), name='post_create'),
     path('create/<int:pk>', PostUpdateView.as_view(), name='post_update'),
--- news/views.py
+++ news/views.py
@@ -19,9 +19,22 @@ class PostList(ListView):
         context = super().get_context_data(**kwargs)
         context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())  # вписываем наш фильтр в контекст
         context['time_now'] = datetime.utcnow()
-       # context['value1'] = None
+        context['value1'] = None
         return context
 
+class PostSearch(ListView):
+    model = Post
+    template_name = 'News_search.html'
+    context_object_name = 'news'
+    queryset = Post.objects.order_by('-dateCreation')
+    paginate_by = 3  # поставим постраничный вывод в один элемент
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())  # вписываем наш фильтр в контекст
+        context['time_now'] = datetime.utcnow()
+        context['value1'] = None
+        return context
 
 class PostDetailView(DetailView):
     model = Post
